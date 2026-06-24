a29843bacb9c5cc4b643eb0f79219e3140730ccf
Ricardo Alfaro <ricardoalfarog@gmail.com>
2026-06-24 13:45:09 -0400

    fix: logo splash y landing cambiado a main_logo_audiodocs_dark.svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
       <div className="hero-bg-circle-1" />
       <div className="hero-bg-circle-2" />
       <img
-        src="/main_logo_audiodocs_white.svg"
+        src="/main_logo_audiodocs_dark.svg"
         alt="Audiodocs"
         className="landing-logo"
       />
